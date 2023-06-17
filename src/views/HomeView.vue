cd94b809c0326ccb50704b13c80a308e63641b23
Steffen Svenningsen <stef7625@gmail.com>
2023-06-17 08:59:54 +0200

    Update stylesheet structure

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,8 +3,12 @@
 
 <template>
   <main>
-    Main - Homepage
+    <h1>Homepage</h1>
   </main>
 </template>
 
-<style lang="sass" scoped></style>
+<style lang="sass" scoped>
+@import @/assets/main.sass
+
+
+</style>
