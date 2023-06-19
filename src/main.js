ec71e401324a2d5c662927ed79a618cc3d6794b5
Steffen Svenningsen <stef7625@gmail.com>
2023-06-19 14:28:49 +0200

    Install and add vueuse motion to the project

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import VueKinesis from "vue-kinesis"
+import { MotionPlugin } from '@vueuse/motion'
 
 import "@/assets/main.sass"
 import "reset-css"
 
 const app = createApp(App)
 
+app.use(MotionPlugin)
 app.use(VueKinesis)
 app.use(router)
 
