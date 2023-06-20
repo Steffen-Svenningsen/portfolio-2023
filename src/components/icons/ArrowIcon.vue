0cfb06919ab90e37f0918cd7853507e8e2f15e39
Steffen Svenningsen <stef7625@gmail.com>
2023-06-20 14:18:25 +0200

    Add arrow link icon

---
+++ src/components/icons/ArrowIcon.vue
@@ -0,0 +1,11 @@
+<template>
+    <svg class="arrow-icon" width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+        <rect width="32" height="32" fill="url(#pattern0)"/>
+        <defs>
+        <pattern id="pattern0" patternContentUnits="objectBoundingBox" width="1" height="1">
+        <use xlink:href="#image0_41_2" transform="translate(-0.166667 -0.166667) scale(0.0208333)"/>
+        </pattern>
+        <image id="image0_41_2" width="64" height="64" xlink:href="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAACXBIWXMAAAsTAAALEwEAmpwYAAACCUlEQVR4nO2aTU7DMBCFvWEJXIOfu4AqQAg4CCs4AVDEZRBco+XnDq26cN7zglYKGrVIxSptIzvEceeTvB3Pex6PE2mMURRFURRFWZOiKA4BdEn2AYBk2eSa5SC5PFhrD0xdlGW5BeAJwKRp0UvMmAB4lFyjiyf52rTACuslqgkAnhIQVbUaujHv/MTb4I3k0WAw2DYNIzmQPCb57hkwttbuB2+AacP7JX44HO6YxBiNRrskPzwT7oMDc3ra8wYcm0Qh2fFy7cUIaueDplD2fyGV6Rlgg4PSay6x44WuuvM1agC1Akq9Aox4pyrinDuTNz2bHhBTfNYGOOfOffGLzMjSAJKnC8RPiqK4yt4ALhG/Tj6tNoArxGdtANcQn60BXFN8lgawgvjsDGBF8VkZwL/FX4bk0woDOBX/VVV8FgYwQHzrDWCg+FYbQPIkVHxrDWAk8a00gBHF/0e+0QMCuPXEj+U/39REcgYIAK5/Tt45d2FqJEkDBAA38gqYmknWgP9CDaBWQKlXgNoDSm2C1Feg1GeQcb4D7KYPSPQ3ekQGwIMX9F0GkkyaQ1KfXq53wYGttQcLxuRkGquTwrTYrOw7vvjZmNxelE0APPqfmKmvKCNy3qjsS9OiKqznWuaFAXRXDSw0fOpj6VnRxc8j46dSXtJhARQJiJYcetLwot15RVEURVHMJvANjgxvK0l2+lcAAAAASUVORK5CYII="/>
+        </defs>
+    </svg>
+</template>
