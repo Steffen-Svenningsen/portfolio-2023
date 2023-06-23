b15e9573febc04008dc36b949ecc9e9e3ceb72af
Steffen Svenningsen <stef7625@gmail.com>
2023-06-23 12:20:24 +0200

    Uninstall and remove vue-kinesis

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import VueKinesis from "vue-kinesis"
 import { MotionPlugin } from '@vueuse/motion'
 
 import "@/assets/main.sass"
@@ -10,7 +9,6 @@ import "reset-css"
 const app = createApp(App)
 
 app.use(MotionPlugin)
-app.use(VueKinesis)
 app.use(router)
 
 app.mount('#app')
